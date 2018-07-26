4943ff133e9426af03c2dc0e8d271b80298d3771
Sh4Kaa <darioo_dario@hotmail.com>
2018-07-26 17:29:24 -0300

    primeiro commit

---
+++ test-vs2017/test-vs2017/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_vs2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá mundooo");
+            Console.ReadKey();
+
+        }
+    }
+}
