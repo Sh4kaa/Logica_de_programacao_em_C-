82caf90d2e21c96c0f245953546ec0b40797d31e
Sh4Kaa <darioo_dario@hotmail.com>
2018-07-27 18:06:20 -0300

    EXERCICIO RESOLVIDO CALCULO DE LARGURA E AREA

---
+++ EXERCICIO RESOLVIDO/EXERCICIO RESOLVIDO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_RESOLVIDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura  ;
+            double comprimento  ;           
+            double area ;
+            double precoMetroQuadrado, preco ;
+
+            largura =Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+             area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("AREA=" + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO=" + preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
