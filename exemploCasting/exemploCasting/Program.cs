f86fa24bce9c2faa12015770cb15ff677cdf14fb
Sh4Kaa <darioo_dario@hotmail.com>
2018-07-27 14:27:54 -0300

    exemplos de casting

---
+++ exemploCasting/exemploCasting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemploCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* int a, b;
+            double resultado;
+            a = 5;
+            b = 2;
+            resultado = (double) a / b; // casting double
+            Console.WriteLine(resultado);
+            Console.ReadKey();    */
+
+            double a;
+            int b;
+            a = 5.0;
+            b = (int) a; //casting, conversão explicita de um tipo para outro, no caso de double para int
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
