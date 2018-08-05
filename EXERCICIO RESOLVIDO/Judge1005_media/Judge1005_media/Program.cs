72b7341918d41e2e27494f5e729661eda864219c
Sh4kaa <darioo_dario@hotmail.com>
2018-08-05 20:50:50 -0300

    calculo media

---
+++ EXERCICIO RESOLVIDO/Judge1005_media/Judge1005_media/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, MEDIA;
+
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+
+            MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("f5"));
+
+            Console.ReadLine();
+        }
+    }
+}
