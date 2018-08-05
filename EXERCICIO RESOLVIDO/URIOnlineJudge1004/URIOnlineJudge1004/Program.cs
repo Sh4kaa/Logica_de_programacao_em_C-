04548c9713d5060db0874793ca03c6d1cb26ad45
Sh4kaa <darioo_dario@hotmail.com>
2018-08-05 20:25:54 -0300

    Judge1004 calculo produto

---
+++ EXERCICIO RESOLVIDO/URIOnlineJudge1004/URIOnlineJudge1004/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URIOnlineJudge1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PROD, a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            PROD = a * b;
+            Console.WriteLine("PROD = " + PROD);
+            // produto de um numero ele vezes ele mesmo ou entre dois numeros um vezes o outro
+        }
+    }
+}
