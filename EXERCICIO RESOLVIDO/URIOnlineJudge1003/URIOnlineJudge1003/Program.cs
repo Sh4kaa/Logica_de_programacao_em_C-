19cb42a4551abbd2b8c6c3c5faac128650aaa760
Sh4kaa <darioo_dario@hotmail.com>
2018-08-05 20:15:16 -0300

    Judge1003

---
+++ EXERCICIO RESOLVIDO/URIOnlineJudge1003/URIOnlineJudge1003/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URIOnlineJudge1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, SOMA;
+            A = Int32.Parse(Console.ReadLine());
+            B = Int32.Parse(Console.ReadLine());// poderia ser usado também "int.Parse" na conversão de String pra inteiro
+            SOMA = A + B;
+            Console.WriteLine("SOMA = "+SOMA);            
+
+        }
+    }
+}
