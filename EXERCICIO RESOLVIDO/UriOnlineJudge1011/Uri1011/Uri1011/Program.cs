4480a8a5d6c596927bbb95475b97b8cee434fc7b
Sh4kaa <darioo_dario@hotmail.com>
2018-08-09 16:16:23 -0300

    calculo volume 1011

---
+++ EXERCICIO RESOLVIDO/UriOnlineJudge1011/Uri1011/Uri1011/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double volume, raio, pi = 3.14159;
+            raio = double.Parse(Console.ReadLine());
+            volume = (4.0 / 3.0) * pi * Math.Pow(raio, 3.0);
+            Console.WriteLine("VOLUME = " + volume.ToString("f3", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
