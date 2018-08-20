f7206a5a22c87bdc7158b2c7ec7659f65bd9990d
Sh4kaa <darioo_dario@hotmail.com>
2018-08-20 15:44:33 -0300

    consumo medio de km/l

---
+++ EXERCICIO RESOLVIDO/uri1014/uri1014/uri1014/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double y, consumo;
+
+            x = int.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumo = (double) x / y;
+
+
+
+
+            Console.WriteLine(consumo.ToString("f3", CultureInfo.InvariantCulture) + " km/l");
+            Console.ReadLine();
+
+
+        }
+    }
+}
