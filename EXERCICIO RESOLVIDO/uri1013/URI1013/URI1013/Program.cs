ae18d5b1ddb8fe38a3b5007978b7bce376815c19
Sh4kaa <darioo_dario@hotmail.com>
2018-08-20 14:57:50 -0300

    maiorAB UriOnline 1013

---
+++ EXERCICIO RESOLVIDO/uri1013/URI1013/URI1013/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maiorAB, a, b, c;
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+
+            //formula maior numero entre 3
+
+             maiorAB = (a + b + Math.Abs(a -b)) / 2;
+             maiorAB = (c + maiorAB + Math.Abs(maiorAB - c)) / 2;
+
+            // AB = (a + b + (abs * (a - b))) / 2;
+
+
+            Console.WriteLine(maiorAB +" eh o maior");
+            Console.ReadLine();
+
+            /* explicação da fórmula
+
+            a b c
+            5 6 7
+
+            5 + 6 = 11
+
+            numero absoluto 5 - 6 = -1
+
+            abs = 1
+
+            formula 5 + 6 + 1 = 12 / 2 = 6
+            maiorAB = 6
+            maiorAB = 7 + 6 + (6 - 7) = 14 / 2 = 7
+
+            maiorab = 7 */
+
+
+        }
+    }
+}
