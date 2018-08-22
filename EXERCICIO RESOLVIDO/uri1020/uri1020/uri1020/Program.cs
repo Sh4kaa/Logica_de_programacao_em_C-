5d75d63a78be87f305324db8a15b47746ebc6d9b
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-22 17:39:04 -0300

    uri1020 Idade em dias

---
+++ EXERCICIO RESOLVIDO/uri1020/uri1020/uri1020/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeDias, anos, meses, dias;
+            int resto;
+            idadeDias = int.Parse(Console.ReadLine());
+
+            anos = idadeDias / 365;
+            resto = idadeDias % 365;
+
+            meses = resto / 30;
+            dias = resto % 30;
+
+
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
