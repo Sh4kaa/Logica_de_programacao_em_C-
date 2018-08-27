a7f3739c0396c9f7072f36420418a194fffddd50
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-27 12:11:28 -0300

    bhaskara uri 1036

--- EXERCICIO RESOLVIDO/baskara1036/baskara1036/Program.cs
+++ EXERCICIO RESOLVIDO/baskara1036/baskara1036/Program.cs
@@ -11,21 +11,28 @@ namespace baskara1036
     {
         static void Main(string[] args)
         {
-            double a, b, c, delta;
+            double a, b, c, delta, r1, r2;
+            String[] vet;
 
-            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
             delta = Math.Pow(b, 2.0) - 4 * a * c;
 
             if ( a == 0 || delta < 0.0)
             {
-                Console.WriteLine("Impossivel de ser calcular");
+                Console.WriteLine("Impossivel calcular");
             } else
             {
-
+                r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("R1 = " + r1.ToString("f5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("f5", CultureInfo.InvariantCulture));
             }
+            Console.ReadLine();
 
 
         }
