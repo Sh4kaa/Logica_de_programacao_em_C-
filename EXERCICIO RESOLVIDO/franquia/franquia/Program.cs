839927f45d7fed1327d258348240ffb838710359
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-27 18:14:09 -0300

    franquia de minutos

---
+++ EXERCICIO RESOLVIDO/franquia/franquia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace franquia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100)
+            {
+                conta = conta + (minutos - 100) * 2.0;
+            }
+            Console.WriteLine("Valor à Pagar R$ " + conta.ToString("f2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
