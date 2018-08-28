5820876f1aad735a6c41a2f6ecab7c8f16f42f93
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-28 09:56:12 -0300

    maior numero entre 3

---
+++ EXERCICIO RESOLVIDO/testemaiornumero/testemaiornumero/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testemaiornumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Digite um numero: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um numero: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um numero: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("MAior numero é: " + a);
+            } else
+            {
+                if (b > c)
+                {
+                    Console.WriteLine("MAior numero é: " + b);
+                } else
+                {
+                    Console.WriteLine("MAior numero é: " + c);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
