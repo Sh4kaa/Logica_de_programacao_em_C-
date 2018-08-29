155fa262233d89ab6edd5d7fc4c04702cdbc0e57
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-29 11:54:42 -0300

    switch case

---
+++ EXERCICIO RESOLVIDO/switchcase/switchcase/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string dia;
+
+            x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1:
+                    dia = "segunda";
+                    break;
+                case 2:
+                    dia = "terçca";
+                    break;
+                case 3:
+                    dia = "quarta";
+                    break;
+                case 4:
+                    dia = "quinta";
+                    break;
+                case 5:
+                    dia = "sexta";
+                    break;
+                case 6:
+                    dia = "sabado";
+                    break;
+                case 7:
+                    dia = "Domingo";
+                    break;
+                default:
+                    dia = "valor inválido";
+                    break;
+
+            }
+            Console.WriteLine("O dia da semana é " + dia);
+            Console.ReadLine();
+        }
+    }
+}
