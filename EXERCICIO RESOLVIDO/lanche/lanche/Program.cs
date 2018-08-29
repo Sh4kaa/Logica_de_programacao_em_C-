0fff8f5c3de6d6dda1bc56e47982eced6e51a4a3
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-29 18:15:37 -0300

    uri lanche em andamento

---
+++ EXERCICIO RESOLVIDO/lanche/lanche/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lanche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cod1=4.00, cod2=4.50, cod3=5.00, cod4=2.00, cod5=1.50;
+            double totalPagar, codigo;
+            int qtdItens;
+            string[] vet;
+            
+
+            vet = Console.ReadLine().Split(' ');
+            codigo = int.Parse(vet[0]);
+            if (codigo == cod1)
+            
+
+
+
+        }
+    }
+}
