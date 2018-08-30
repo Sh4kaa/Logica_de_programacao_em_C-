16b5813416039afe39beeed6cb2e5e93ba9062c0
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-30 09:51:12 -0300

    uri 1038 lanche

--- EXERCICIO RESOLVIDO/lanche/lanche/Program.cs
+++ EXERCICIO RESOLVIDO/lanche/lanche/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,53 @@ namespace lanche
     {
         static void Main(string[] args)
         {
-            double cod1=4.00, cod2=4.50, cod3=5.00, cod4=2.00, cod5=1.50;
-            double totalPagar, codigo;
-            int qtdItens;
+            double cachorroQuente=4.00, xSalada=4.50, xBacon=5.00, torradaSimples=2.00, refrigerente=1.50;
+            double totalPagar;
+            int qtdItens, codigo;
             string[] vet;
             
 
             vet = Console.ReadLine().Split(' ');
             codigo = int.Parse(vet[0]);
-            if (codigo == cod1)
-            
+            qtdItens = int.Parse(vet[1]);
+
+            if (codigo == 1)
+            {
+               totalPagar = cachorroQuente * qtdItens;
+               Console.WriteLine("Total: R$ " + totalPagar.ToString("f2", CultureInfo.InvariantCulture));
+            } else
+            {
+                if ( codigo == 2)
+                {
+                    totalPagar = xSalada * qtdItens;
+                    Console.WriteLine("Total: R$ " + totalPagar.ToString("f2", CultureInfo.InvariantCulture));
+                } else
+                {
+                    if ( codigo == 3)
+                    {
+                        totalPagar = xBacon * qtdItens;
+                        Console.WriteLine("Total: R$ " + totalPagar.ToString("f2", CultureInfo.InvariantCulture));
+                    } else
+                    {
+                        if ( codigo == 4)
+                        {
+                            totalPagar = torradaSimples * qtdItens;
+                            Console.WriteLine("Total: R$ " + totalPagar.ToString("f2", CultureInfo.InvariantCulture));
+                        } else
+                        {
+                            if ( codigo == 5)
+                            {
+                                totalPagar = refrigerente * qtdItens;
+                                Console.WriteLine("Total: R$ " + totalPagar.ToString("f2", CultureInfo.InvariantCulture));
+                            }
+                        }
+                    }
+                    
+
+                }
+            }
+
+            Console.ReadLine();
 
 
 
