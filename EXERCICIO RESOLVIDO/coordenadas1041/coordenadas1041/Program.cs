46911be327120456a72e6947a9413cc40a694d53
Sh4Kaa <darioo_dario@hotmail.com>
2018-08-30 13:48:17 -0300

    uri1041 coordenadas de um ponto

---
+++ EXERCICIO RESOLVIDO/coordenadas1041/coordenadas1041/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace coordenadas1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;            
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            if (x > 0 && y > 0 )
+            {
+                Console.WriteLine("Q1");
+            } else if (x < 0 && y > 0 )
+            {
+                Console.WriteLine("Q2");
+            } else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            } else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            } else if ( x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            } else if (x == 0 && y != x)
+            {
+                Console.WriteLine("Eixo Y");
+            } else if (y == 0 && x != 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            /*se x for igual a zero e y diferente de zero, o ponto está no eixo y
+             * e vice versa*/
+            Console.ReadLine();
+            
+        }
+    }
+}
