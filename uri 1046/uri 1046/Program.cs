38fcdf9a10829b298f82ded88928bc010a2e5c67
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-03 16:31:58 -0300

    uri1046 horas

---
+++ uri 1046/uri 1046/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x , y, tempo;
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            tempo = (24 - x) + y;
+            if ( tempo > 24)
+            {
+                tempo = tempo - 24;
+                Console.WriteLine("O JOGO DUROU " + tempo + " HORA(S)");
+            } else
+            {
+                Console.WriteLine("O JOGO DUROU " + tempo + " HORA(S)");
+            }
+            Console.ReadLine();            
+
+            
+
+                        
+        }
+    }
+}
