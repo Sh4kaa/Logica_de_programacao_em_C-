e6987f6ac6747d45d4d8699db000c48afc6f0f51
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-06 14:00:29 -0300

    uri1051 correto mas com erro compilation error no uri

--- EXERCICIO RESOLVIDO/uri1051/Program.cs
+++ EXERCICIO RESOLVIDO/uri1051/Program.cs
@@ -11,39 +11,37 @@ namespace uri1051
     {
         static void Main(string[] args)
         {
-            double salario, impostoRenda, diferenca;
+            double salario, imposto=0, diferenca;
+
             salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            if (salario <= 2000)
+            if (salario > 4500)
+            {
+                imposto = 1000 * 8 / 100 + 1500 * 18 / 100;
+                diferenca = salario - 4500;
+                imposto += diferenca * 28 / 100;
+                Console.WriteLine("R$ " + imposto.ToString("f2", CultureInfo.InvariantCulture));
+            } else if ( salario > 3000)
+            {
+                imposto = 1000 * 8 / 100;
+                diferenca = salario - 3000;
+                imposto += diferenca * 18 / 100;
+                Console.WriteLine("R$ " + imposto.ToString("f2", CultureInfo.InvariantCulture));
+            } else if (salario > 2000)
+            {
+                diferenca = salario - 2000;
+                imposto += diferenca * 8 / 100;
+                Console.WriteLine("R$ " + imposto.ToString("f2", CultureInfo.InvariantCulture));
+
+            } else
             {
                 Console.WriteLine("Isento");
             }
-            else if (salario > 3000)
-            {
-                impostoRenda = 1000 * 8 / 100;
-                diferenca = salario - 3000;
-                impostoRenda +=  diferenca * 18 / 100;                
-                Console.WriteLine(impostoRenda.ToString("f2", CultureInfo.InvariantCulture));
-            } else if ( salario >= 4500)
-            {
-                impostoRenda = 1500 * 18 / 100;
-                diferenca = salario - 4500;
-                impostoRenda += diferenca * 28 / 100;
-                Console.WriteLine(impostoRenda.ToString("f2", CultureInfo.InvariantCulture));
-            } else if (salario > 4500)
-            {
-                impostoRenda = 1000 * 0.08 + 1500 * 0.18;
-                diferenca = salario - 4500;
-                impostoRenda += diferenca * 28 / 100;
-                Console.WriteLine(impostoRenda.ToString("f2", CultureInfo.InvariantCulture));
-            }
             
-
-            
-
-            
-        
-            Console.ReadLine();
+            Console.ReadLine();        
+                                
+                    
+                      
         }
     }
 }
