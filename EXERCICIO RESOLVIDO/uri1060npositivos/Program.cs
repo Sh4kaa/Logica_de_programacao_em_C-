adcc4277421bbb082e7ff22dcb5cc67d5bf1f484
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-11 18:11:55 -0300

    uri 1060 aceito numero positivos

---
+++ EXERCICIO RESOLVIDO/uri1060npositivos/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1060npositivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            int positivo = 0;
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if ( n >= 0)
+            {
+                positivo++;
+            }
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+            }
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+            }
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+            }
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+            }
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+            }
+
+            Console.WriteLine(positivo + " valores positivos");
+            Console.ReadLine();
+
+        }
+    }
+}
