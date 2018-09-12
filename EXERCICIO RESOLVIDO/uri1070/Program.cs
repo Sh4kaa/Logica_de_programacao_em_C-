7b77e50eaa165bc53898cc7c675af81b88ed4daf
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-12 16:29:09 -0300

    uri 1070

---
+++ EXERCICIO RESOLVIDO/uri1070/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = int.Parse(Console.ReadLine());
+
+            if (x % 2 != 0)
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+                
+            }
+            else
+            {
+                Console.WriteLine(x += 1);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+                Console.WriteLine(x += 2);
+
+            }         
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
