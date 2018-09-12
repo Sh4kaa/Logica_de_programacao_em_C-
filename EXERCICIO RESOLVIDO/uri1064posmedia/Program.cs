b2c57944e9f8d3627c435092e00c274f4312a3a9
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-12 12:08:16 -0300

    uri1064  numeros positivos e media

---
+++ EXERCICIO RESOLVIDO/uri1064posmedia/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1064posmedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, soma=0;
+            int positivo = 0, contador=0;
+
+
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+                soma =  n + soma;
+                contador++;
+            }
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+                soma = n + soma;
+                contador++;
+            }
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+                soma = n + soma;
+                contador++;
+            }
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+                soma = n + soma;
+                contador++;
+            }
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+                soma = n + soma;
+                contador++;
+            }
+            n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (n >= 0)
+            {
+                positivo++;
+                soma = n + soma;
+                contador++;
+            }
+            Console.WriteLine(positivo + " valores positivos");
+            Console.WriteLine((soma / contador).ToString("f1", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
