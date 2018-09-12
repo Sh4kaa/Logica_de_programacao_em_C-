224aa62982b57b5f52df3261174f940aea05cfa3
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-12 14:29:23 -0300

    1066 uri, contagem numeros pares, impares, negativos e positivos

---
+++ EXERCICIO RESOLVIDO/uri1066/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int contPar=0, contImpar=0, contPos=0, contNeg=0, n;
+            
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contPar++;
+            } else
+            {
+                contImpar++;
+            }
+            if (n < 0 )
+            {
+                contNeg++;
+            } else if (n > 0)
+            {
+                contPos++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contPar++;
+            }
+            else
+            {
+                contImpar++;
+            }
+            if (n < 0)
+            {
+                contNeg++;
+            }
+            else if (n > 0)
+            {
+                contPos++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contPar++;
+            }
+            else
+            {
+                contImpar++;
+            }
+            if (n < 0)
+            {
+                contNeg++;
+            }
+            else if (n > 0)
+            {
+                contPos++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contPar++;
+            }
+            else
+            {
+                contImpar++;
+            }
+            if (n < 0)
+            {
+                contNeg++;
+            }
+            else if (n > 0)
+            {
+                contPos++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contPar++;
+            }
+            else
+            {
+                contImpar++;
+            }
+            if (n < 0)
+            {
+                contNeg++;
+            }
+            else if (n > 0)
+            {
+                contPos++;
+            }
+
+
+
+            Console.WriteLine(contPar + " valor(es) par(es)");
+            Console.WriteLine(contImpar + " valor(es) impar(es)");
+            Console.WriteLine(contPos + " valor(es) positivo(s)");
+            Console.WriteLine(contNeg + " valor(es) negativo(s)");
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
