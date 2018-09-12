3083e5e21fd2c35c58f5494f70e9a2866bc2f347
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-12 13:42:07 -0300

    uri 1065

---
+++ EXERCICIO RESOLVIDO/uri 1065 pares/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri_1065_pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, contador=0;
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contador ++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contador++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contador++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contador++;
+            }
+
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                contador++;
+            }
+
+            Console.WriteLine(contador + " valores pares");
+            Console.ReadLine();
+        }
+    }
+}
