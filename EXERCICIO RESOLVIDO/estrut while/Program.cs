148c2f5011624b08f69badf8f936f46065e2ac15
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-17 15:01:03 -0300

    estrutura while

---
+++ EXERCICIO RESOLVIDO/estrut while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estrut_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, soma=0;
+            n = int.Parse(Console.ReadLine());
+            
+            while (n != 0)
+            {
+                soma = soma + n;
+                n = int.Parse(Console.ReadLine());                
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
