bdc0df4c674d358a67f0909c0fd95d7be675439b
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-18 16:14:15 -0300

    uri1114 senha fixa

---
+++ EXERCICIO RESOLVIDO/uri1114/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+            Console.ReadLine();
+            
+        }
+    }
+}
