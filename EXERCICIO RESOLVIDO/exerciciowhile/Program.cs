822e06d942b071656b2896518e4127ab38ea788d
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-18 15:48:06 -0300

    exemplo whileee

---
+++ EXERCICIO RESOLVIDO/exerciciowhile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace exerciciowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double idade, soma, media;
+            int cont;
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+            cont = 0;
+            while (idade >= 0)
+            {
+                soma += idade;
+                cont = cont + 1;                
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            if (cont == 0)
+            {
+                Console.WriteLine("imposivel calcular");
+            }
+            else
+            {
+
+                media = soma / cont;
+                Console.WriteLine(media.ToString("f2", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
