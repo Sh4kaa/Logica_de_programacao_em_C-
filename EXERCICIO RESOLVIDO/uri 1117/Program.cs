bb02a57b93fb8eaec2d707f9e2bc3e4581266e29
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-20 18:08:32 -0300

    uri 1117 em andamento

---
+++ EXERCICIO RESOLVIDO/uri 1117/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri_1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota, soma = 0, media;
+            int cont = 0;
+
+            nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (cont < 2)
+            {
+                if (nota < 0)
+                {
+                    Console.WriteLine("nota invalida");
+                }
+                else if (nota > 0 && nota <= 10)
+                {
+                    soma = nota + soma;
+                    cont++;
+
+                }
+                nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            media = soma / cont;
+            Console.WriteLine("media = " + media.ToString("f2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
