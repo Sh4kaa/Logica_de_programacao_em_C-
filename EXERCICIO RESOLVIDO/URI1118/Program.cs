ac35413398675813973e0f38a39c6fd40151c9a5
Sh4Kaa <darioo_dario@hotmail.com>
2018-09-28 13:54:26 -0300

    uri 1118 em andamento

--- EXERCICIO RESOLVIDO/URI1118/Program.cs
+++ EXERCICIO RESOLVIDO/URI1118/Program.cs
@@ -37,24 +37,27 @@ namespace URI1118
             Console.WriteLine("novo calculo (1-sim 2-nao)");
             x = int.Parse(Console.ReadLine());
 
-
-            while (x <= 1 || x > 2)
+            if ( x < 1 || x != 2)
             {
-                
-                    Console.WriteLine("novo calculo (1-sim 2-nao)");
-                    x = int.Parse(Console.ReadLine());
-                
-                    
-                
+                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                while (nota1 < 0 || nota1 > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                    nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            }
+                }
 
-            Console.WriteLine("media = " + media.ToString("f2", CultureInfo.InvariantCulture));
+                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                while (nota2 < 0 || nota2 > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                    nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
 
+                media = (nota1 + nota2) / 2;
+                Console.WriteLine("media = " + media.ToString("f2", CultureInfo.InvariantCulture));
 
-
-
-
+            }          
             Console.ReadLine();
 
 
