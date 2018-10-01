6b6dc7b3daf631f62ea3a30e2561c016d022449b
Sh4Kaa <darioo_dario@hotmail.com>
2018-10-01 18:13:17 -0300

    uri 1118

--- EXERCICIO RESOLVIDO/URI1118/Program.cs
+++ EXERCICIO RESOLVIDO/URI1118/Program.cs
@@ -1,68 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace URI1118
+namespace _1118
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double nota1, nota2, media, soma = 0;
-            int x = 0;
 
+            int novoCalculo;
 
-            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            while (nota1 < 0 || nota1 > 10)
+            double n1, n2, media;
+
+            novoCalculo = 1;
+
+            while (novoCalculo == 1)
             {
-                Console.WriteLine("nota invalida");
-                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            }
+                n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            while (nota2 < 0 || nota2 > 10)
-            {
-                Console.WriteLine("nota invalida");
-                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            }
-
-            media = (nota1 + nota2) / 2;
-            Console.WriteLine("media = " + media.ToString("f2", CultureInfo.InvariantCulture));
-
-
-            Console.WriteLine("novo calculo (1-sim 2-nao)");
-            x = int.Parse(Console.ReadLine());
-
-            if ( x < 1 || x != 2)
-            {
-                nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                while (nota1 < 0 || nota1 > 10)
+                while (n1 < 0.0 || n1 > 10.0)
                 {
+
                     Console.WriteLine("nota invalida");
-                    nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+                n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                while (n2 < 0.0 || n2 > 10.0)
+                {
+
+                    Console.WriteLine("nota invalida");
+
+                    n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+                media = (n1 + n2) / 2;
+
+                Console.WriteLine("media = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.WriteLine("novo calculo (1-sim 2-nao)");
+                novoCalculo = int.Parse(Console.ReadLine());
+
+                while (novoCalculo != 1 && novoCalculo != 2)
+                {
+
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    novoCalculo = int.Parse(Console.ReadLine());
 
                 }
 
-                nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                while (nota2 < 0 || nota2 > 10)
-                {
-                    Console.WriteLine("nota invalida");
-                    nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                }
-
-                media = (nota1 + nota2) / 2;
-                Console.WriteLine("media = " + media.ToString("f2", CultureInfo.InvariantCulture));
-
-            }          
+            }
             Console.ReadLine();
-
-
-
-
 
         }
     }
