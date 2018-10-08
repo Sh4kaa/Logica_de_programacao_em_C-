c184b7deab04f92f58c052d30bd194241408dc92
Sh4Kaa <darioo_dario@hotmail.com>
2018-10-08 16:41:24 -0300

    Uri 1154 idade

---
+++ EXERCICIO RESOLVIDO/uri 1154/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri_1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0, cont = 0;
+            double media=0.0;
+
+            while (idade >= 0)
+            {
+                idade = int.Parse(Console.ReadLine());
+                if (idade >= 0)
+                {
+                    media += idade;
+                    cont++;
+                }
+            }
+            
+            Console.WriteLine((media / cont).ToString("f2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
