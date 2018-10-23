01ccc400c6d1ee4495d572413957c07ff974486c
Sh4Kaa <darioo_dario@hotmail.com>
2018-10-23 17:34:56 -0300

    exerccio "for"

---
+++ EXERCICIO RESOLVIDO/exercicioprof/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioprof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int n, x, soma=0;
+            n = int.Parse(Console.ReadLine());
+            for(int i=1; i <= n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma += x;
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();*/
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Valor de i: " + i);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ EXERCICIO RESOLVIDO/uri 1159/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri_1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma=0, cont=0, sub=0;
+            x = 1;
+
+            while (x != 0)
+            {
+                x = int.Parse(Console.ReadLine());
+                if ( x % 2 == 0)
+                {
+                    soma += x;
+                    while (cont < 4)
+                    {               
+                                                             
+                        cont++;
+                        soma += 2;
+                    }                       
+                                    
+                } else
+                {
+                    sub += 1;
+                    while (cont < 5)
+                    {
+
+                        sub += 2;
+                        cont++;
+                        soma += sub;
+
+                    }
+                }
+                
+                Console.WriteLine(soma);
+
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
