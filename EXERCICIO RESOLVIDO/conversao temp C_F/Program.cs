bb259dc45a9958c5530ef87740254f0a11aad430
Dario <darioo_dario@hotmail.com>
2018-12-27 13:29:17 -0200

    Conversão  Celsius para Fahrenheit

---
+++ EXERCICIO RESOLVIDO/conversao temp C_F/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversao_temp_C_F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double C, F;
+            char resposta= 's';
+
+            while (resposta == 's')
+            {
+                Console.Write("Digite a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("f1", CultureInfo.InvariantCulture));
+
+                Console.Write("Deseja repetir (s/n)?");
+                resposta = char.Parse(Console.ReadLine());
+            }            
+            
+            
+            Console.ReadLine();
+
+        }
+    }
+}
