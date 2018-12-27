88850a9dd590f2bdc1635cdeb04affede26f9f4e
Dario <darioo_dario@hotmail.com>
2018-12-27 10:25:57 -0200

    uri 1078 tabuada

---
+++ EXERCICIO RESOLVIDO/uri1078/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = i * n;
+                Console.WriteLine(n + " x " + i + " = " + resultado);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
