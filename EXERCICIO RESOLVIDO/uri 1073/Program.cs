b050059944f8587c53cf108cf3aee47693aa3c2b
Sh4Kaa <darioo_dario@hotmail.com>
2019-01-02 17:42:42 -0200

    em andamento uri 1073

---
+++ EXERCICIO RESOLVIDO/uri 1073/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uri_1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, result=0;
+            n = int.Parse(Console.ReadLine());
+
+
+            if (n % 2 == 0)
+            {
+                result = n * 2;
+                Console.WriteLine(n + "^2 = " + result);
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+              
+                if (i % 2 == 0)
+                {
+                    
+                    result = i * 2;
+                    Console.WriteLine(i + "^2 = " + result);
+                }
+
+            }
+            Console.ReadLine();
+
+
+
+
+           
+        }
+    }
+}
