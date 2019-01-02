3000b8afca516b5f1306117b98dd72e146d8cd95
Sh4Kaa <darioo_dario@hotmail.com>
2019-01-02 16:38:53 -0200

    uri 1072 intervalo 2

---
+++ EXERCICIO RESOLVIDO/1072 uri/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1072_uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, fora=0, dentro=0;
+            
+
+            n = int.Parse(Console.ReadLine());
+            x = 0;
+            
+            
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+                
+
+            }
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
