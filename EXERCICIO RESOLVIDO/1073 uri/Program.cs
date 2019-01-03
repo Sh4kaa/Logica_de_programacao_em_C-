e74dc8ef2ac0233837865e62c3f8afd279cfd26a
Dario <darioo_dario@hotmail.com>
2019-01-03 14:52:00 -0200

    for
    1073 exercicio uri ok

---
+++ EXERCICIO RESOLVIDO/1073 uri/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1073_uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, result = 0;
+            
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    result = i * i;
+                    Console.WriteLine(i + "^2 = " + result);
+                }
+            }            
+            Console.ReadLine();
+        }
+    }
+}
