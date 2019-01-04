f1f01a61e5d9e8ed99940c736df0d9fe0b99ced8
Dario <darioo_dario@hotmail.com>
2019-01-04 15:43:12 -0200

    uri 1075
    media ponderada com o "for"
    exercicio uri

---
+++ EXERCICIO RESOLVIDO/1075 uri/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1075_uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double v1, v2, v3, media, peso;            
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)            {
+
+                string[] vet = Console.ReadLine().Split(' ');
+                v1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                v2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                v3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+                peso = v1 * 2.0 + v2 * 3.0 + v3 * 5.0;
+                media = peso / 10.0;
+                Console.WriteLine(media.ToString("f1", CultureInfo.InvariantCulture));
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
