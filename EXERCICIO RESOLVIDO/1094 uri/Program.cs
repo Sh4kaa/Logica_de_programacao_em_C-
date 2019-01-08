a1b267b70298fff3bf5d26aed1a5eb2e298c3c46
Sh4Kaa <darioo_dario@hotmail.com>
2019-01-08 16:06:55 -0200

    en andamento

---
+++ EXERCICIO RESOLVIDO/1094 uri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1094_uri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, quantidade=0, C, R, S;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+              if (n <= 1 && n <= 15)
+                {
+                    n = int.Parse(Console.ReadLine());
+                    quantidade++;
+                    
+                }   
+            }
+        }
+    }
+}
