c69f632ab5f33125aae94e314210ff8e1b5b3ad7
Sh4Kaa <darioo_dario@hotmail.com>
2019-01-08 15:26:27 -0200

    uri 1080 maior e posição

---
+++ EXERCICIO RESOLVIDO/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uri1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, maior=0, posicao=0;
+
+            for (int i=1; i <= 100; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                if( n >= maior)
+                {
+                    maior = n;
+                    posicao = i;
+                    
+                }
+                
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+            Console.ReadLine();
+        }
+    }
+}
