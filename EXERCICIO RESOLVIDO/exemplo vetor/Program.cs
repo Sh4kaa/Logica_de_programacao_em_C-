b7f955817ac74cf79dceb1d1a1adaf379aba5a2e
Dario <darioo_dario@hotmail.com>
2019-01-14 17:10:27 -0200

    exemplo de vetor
    curso da udemy de lógica

---
+++ EXERCICIO RESOLVIDO/exemplo vetor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace exemplo_vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double[] vet;            
+            n = int.Parse(Console.ReadLine());
+            vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vet[i].ToString("f1", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+        }
+    }
+}
