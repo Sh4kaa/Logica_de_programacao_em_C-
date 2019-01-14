981444525a5522099f6b2bf489764d6ab6dd9d04
Dario <darioo_dario@hotmail.com>
2019-01-14 15:12:20 -0200

    uri 1145
    exercicio chato demais

---
+++ EXERCICIO RESOLVIDO/teste5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, aux=0;
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            for (int i = 1; i <= y; i++)
+            {
+                Console.Write(i);
+                aux += 1;
+                if (aux == x)
+                {
+                    Console.WriteLine();
+                    aux = 0;
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
