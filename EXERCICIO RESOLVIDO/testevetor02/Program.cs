149b45410b96de15c57eaef6cbdde0b936940495
Dario <darioo_dario@hotmail.com>
2019-01-14 18:01:06 -0200

    teste de vetor 02
    exercicio para memorização de uma estrutura de vetor

---
+++ EXERCICIO RESOLVIDO/testevetor02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testevetor02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] vet;
+            n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
