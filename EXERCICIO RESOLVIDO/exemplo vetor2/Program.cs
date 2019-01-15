ec11555872822e5d3c1ea487b43a85c918cb7d6e
Dario <darioo_dario@hotmail.com>
2019-01-15 17:33:01 -0200

    exemplo de vetores 3.1
    exemmplo de vetores um pouco mais complexo que nos exercicios anteriores

---
+++ EXERCICIO RESOLVIDO/exemplo vetor2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace exemplo_vetor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] nomes = new string[N];
+            int[] idades = new int[N];
+            double[] alturas = new double[N];
+
+
+            // leitura dos dados
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nomes[i] = s[0];
+                idades[i] = int.Parse(s[1]);
+                alturas[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+
+            }
+
+            // cálculo da altura  média das pessoas
+
+            double soma = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                soma += alturas[i];
+            }
+
+            double media = soma / N;
+
+            Console.WriteLine("Altura Média: " + media.ToString("f2", CultureInfo.InvariantCulture));
+            // percentual de pessoas com idade com menos de 16 anos
+            int cont = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (idades[i] < 16)
+                {
+                    cont++;
+                }
+            }
+            double porcentagem = (double)cont / N * 100.0;
+
+            Console.WriteLine("Pessoas com menos de 16 anos: " + porcentagem.ToString("f1", CultureInfo.InvariantCulture) + "%");
+            Console.ReadLine();
+
+        }
+    }
+}
--- EXERCICIO RESOLVIDO/vetor exemplo3/Program.cs
+++ EXERCICIO RESOLVIDO/vetor exemplo3/Program.cs
@@ -7,6 +7,7 @@ namespace vetor_exemplo3
     {
         static void Main(string[] args)
         {
+            // declaração de variáveis
             int n;
             double[] a; // declarar o vetor
             double soma = 0.0;
@@ -16,29 +17,37 @@ namespace vetor_exemplo3
 
             string[] s = Console.ReadLine().Split(' ');
 
+            // "for" para leitura dos dados ou entrada de dados
             for (int i = 0; i < n; i++)
             {
                 a[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
-
             }
 
+            // "for" para escrever na tela
             for (int i = 0; i < n; i++)
             {
                 Console.Write(a[i].ToString("f1", CultureInfo.InvariantCulture) + " ");
             }
             Console.WriteLine();
 
+            //"for" para calcular a soma dos dados
             for (int i = 0; i < n; i++)
             {
                 soma += a[i];
             }
 
-            double media = soma / n;
+            double media = soma / n; // declaração variável média com a atribuição.
 
+            // saída de dados
             Console.WriteLine(soma.ToString("f2", CultureInfo.InvariantCulture));
             Console.WriteLine(media.ToString("f2", CultureInfo.InvariantCulture));
 
             Console.ReadLine();
+
+            // considerações
+            /* Esse código poderia ser resumido em apenas um "for", porém por
+             * questões acadêmicas e de bons modos, fica mais claro
+             * entender o código dessa forma, pois fica menos embaralhado */
 
         }
     }
