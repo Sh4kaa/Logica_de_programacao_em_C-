a1feab2247e67f5f57c01a0da138959892eaf0c9
Dario <darioo_dario@hotmail.com>
2019-01-15 15:23:16 -0200

    exemplo de vetor
    este exemplo explica com clareza como funciona um vetor

---
+++ EXERCICIO RESOLVIDO/vetor exemplo3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace vetor_exemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double[] a; // declarar o vetor
+            double soma = 0.0;
+
+            n = int.Parse(Console.ReadLine()); // leitura da variável
+            a = new double[n]; // depois alocar ele na memória
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i].ToString("f1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += a[i];
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine(soma.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("f2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
