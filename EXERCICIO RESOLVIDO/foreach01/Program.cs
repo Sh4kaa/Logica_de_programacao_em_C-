e96fc21a089ab4c6e71f51f5c7ec6d48dae0bd95
Dario <darioo_dario@hotmail.com>
2019-01-16 10:08:18 -0200

    "for" e "foreach"
    exemplos de for e foreach
    for = para
    foreach = faça para cada

---
+++ EXERCICIO RESOLVIDO/foreach01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreach01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] vet = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Nomes lidos:");
+            foreach (string x in vet)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
