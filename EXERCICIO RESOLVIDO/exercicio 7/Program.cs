903daeed6a3eef63b9510eb2f1c3244c295e1856
Dario <darioo_dario@hotmail.com>
2019-02-14 16:57:09 -0200

    exercicio 7
    Fazer um programa para ler um conjunto de N nomes de alunos, bem como as notas que eles tiraram no 1º e 2º semestres.
    Cada uma dessas informações deve ser armazenada em um vetor. Depois, imprimir os nomes dos alunos aprovados,
    considerando aprovados aqueles cuja média das notas seja maior ou igual a seis.

---
+++ EXERCICIO RESOLVIDO/exercicio 7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] alunos = new string[N];
+            double[] sem1 = new double[N];
+            double[] sem2 = new double[N];
+            string[] s;
+            double media;
+            
+            
+
+
+            for (int i = 0; i < N; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                alunos[i] = s[0];
+                sem1[i] = double.Parse(s[1], CultureInfo.InvariantCulture);
+                sem2[i] = double.Parse(s[2], CultureInfo.InvariantCulture);                           
+
+            }
+
+            Console.WriteLine("Alunos aprovados:");
+
+            for (int i = 0; i < N; i++)
+            {
+                media = (sem1[i] + sem2[i]) / 2;
+                if (media >= 6.0)
+                {
+                    Console.WriteLine(alunos[i]);
+                }
+            }
+            Console.ReadLine();
+            
+            
+
+        }
+    }
+}
