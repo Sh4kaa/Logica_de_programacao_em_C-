2cb39eb507b034b79d414853e4a4c5e5466d83d9
Dario <darioo_dario@hotmail.com>
2019-04-29 14:17:22 -0300

    Exercicio 8
    Tem-se um conjunto de dados contendo a altura e o sexo (M, F) de N pessoas. Fazer um programa que calcule e escreva:
     a maior e a menor altura do grupo
     a média de altura das mulheres
     o número de homens

---
+++ EXERCICIO RESOLVIDO/exercicio8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            double maiorAlt = 0, menorAlt, mediaAltMulher, aux=0;
+            double[] altura = new double[N];
+            string[] sexo = new string[N];
+            int numHomens;
+            string[] dados;
+
+            
+
+            for (int i = 0; i < N; i++)
+            {
+                dados = Console.ReadLine().Split(' ');
+                altura[i] = double.Parse(dados[0], CultureInfo.InvariantCulture);                
+                sexo[i] = dados[1];
+
+               
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (altura[i] > maiorAlt)
+                {
+                    maiorAlt += altura[i];
+                }
+            }
+
+            Console.WriteLine(maiorAlt.ToString("f2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
