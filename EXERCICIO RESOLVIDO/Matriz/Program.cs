6fa2f7c3ec165f5986e8bc78f7c67b7933f30a4b
Dario <darioo_dario@hotmail.com>
2019-06-21 12:49:36 -0300

    exercicio de matriz diagonais
    segue exercicio

---
+++ EXERCICIO RESOLVIDO/Matriz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M, N;
+            int[,] A;
+            string[] s1 = Console.ReadLine().Split(' ');
+            M = int.Parse(s1[0]);
+            N = int.Parse(s1[1]);
+
+            A = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = int.Parse(s[j]);
+                }
+            }
+            // impressao na tela
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(A[i, j] + " ");
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EXERCICIO RESOLVIDO/exercicio/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[,] A;
+            N = int.Parse(Console.ReadLine());
+            A = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(A[i, i] + " ");
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS");
+
+            int cont = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+               for(int j = 0; j < N; j++)
+                {
+                    if (A[i, j] < 0)
+                    {
+                        cont++;
+                    }
+                    
+                }
+            }
+            Console.WriteLine(cont++);
+            Console.ReadLine();
+
+        }
+    }
+}
