050021311137489003efe6864c2fa43e3fc4e23a
Sh4Kaa <darioo_dario@hotmail.com>
2019-07-18 17:57:13 -0300

    3) Ler um inteiro N e uma matriz quadrada de ordem N. Mostrar qual o maior elemento de cada linha. Suponha não haver empates

---
+++ ExerciciosMatrizes/exercicio3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            int[,] mat = new int[N,N];
+            string[] s;
+
+            for(int i = 0; i< N; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);                  
+                   
+                }
+                
+            }
+
+            // maio elemeno de cada linha
+
+            for (int i = 0; i < N; i++)
+            {
+                int maior = 0;                
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] > maior)
+                    {
+                        maior = mat[i, j];
+                    }
+                }
+                Console.WriteLine(maior);
+            }
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
