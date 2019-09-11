06951903c0025d7aff8f8de96e114e1019886268
Dario <darioo_dario@hotmail.com>
2019-09-10 22:04:09 -0300

    exercicio 7
    7) Em um tabuleiro de xadrez existe uma torre e quatro peões adversários. Você deve ler uma matriz de caracteres (char) indicando as posições de cada uma dessas peças. Em seguida, você deve gerar uma matriz de valores booleanos (bool) indicando para quais casas é possível a torre se mover: em todas casas para onde a torre pode se mover deve estar o valor true, e nas demais casas o valor false.
    
    Entrada: A entrada consiste da matriz 8x8 contendo o valor de cada peça do tabuleiro, sendo o caractere 'T' para torre, o caractere 'P' para peão, e o caracter '-' para casa desocupada.
    
    Saída: A saída consiste da matriz 8x8 indicando para quais casas a torre pode se mover. As casas para onde a torre pode se mover devem ser representadas por um 'x', e as demais casas por um '-'.

---
+++ ExerciciosMatrizes/exercicio5/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int M, N;
+             M = int.Parse(Console.ReadLine());
+             N = int.Parse(Console.ReadLine());
+             int[,] mat = new int[M, N];
+             string[] s;
+             int[,] vetAux = new int[M, N];
+
+
+             // leitura da matriz
+             for (int i = 0; i < M; i++)
+             {
+
+                 s = Console.ReadLine().Split(' ');
+                 for (int j = 0; j < N; j++)
+                 {
+                     mat[i, j] = int.Parse(s[j]);
+
+                 }
+
+             }
+
+             Console.WriteLine(); // pula linha
+
+             // leitura do numero da fila
+             int numFila;
+             Console.Write("Digite o numero da Fila: ");
+             numFila = int.Parse(Console.ReadLine());
+             numFila--;
+
+             Console.WriteLine();
+
+             // lendo a fila selecionada e jogando no vetor auxiliar  
+
+             for (int i = numFila; i == numFila; i++)
+             {
+
+                 for (int j = 0; j < N; j++)
+                 {
+                     vetAux[i, j] = mat[i, j];
+                 }
+             }
+
+             // mostrando a fila da matriz sem alteração
+
+             for (int i = numFila; i == numFila; i++)
+             {
+
+                 for (int j = 0; j < N; j++)
+                 {
+                     if (vetAux[i,j] == 0)
+                     {
+                         vetAux[i, j] = mat[i, N - 1];
+                         Console.Write(vetAux[i, j]);
+                     }
+
+                 }
+
+             }*/
+
+            int M, N;
+            M = int.Parse(Console.ReadLine());
+            N = int.Parse(Console.ReadLine());
+            int[,] mat = new int[M, N];
+            string[] s;
+
+
+            // leitura da matriz
+            for (int i = 0; i < M; i++)
+            {
+
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(s[j]);
+
+                }
+
+            }
+
+            Console.WriteLine(); // pula linha
+
+            // leitura do numero da fila
+            int numFila;
+            int[] gira = new int[N];
+            numFila = int.Parse(Console.ReadLine());
+            numFila--;
+
+            Console.WriteLine(); //pula linha
+
+            // girando a fila selecionada
+
+            for (int i = 0; i < N; i++)
+            {
+                if (i == N - 1) { gira[0] = mat[numFila, i]; }
+                else { gira[i + 1] = mat[numFila, i]; }
+            }
+
+            // devolvendo a fila alterada para matriz
+            for (int j = 0; j < N; j++)
+            {
+                mat[numFila, j] = gira[j];
+            }
+
+            // mostra a matriz alterada
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
