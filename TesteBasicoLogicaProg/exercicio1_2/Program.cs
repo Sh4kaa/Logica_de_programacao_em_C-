acda5e1fd121a4f571606fa65416eb8bd407ce6c
Dario <darioo_dario@hotmail.com>
2019-09-12 20:42:45 -0300

    exercicio 1.2
    Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos.
     Fórmula: = .
     Considere o valor de π = 3.14159

---
+++ TesteBasicoLogicaProg/exercicio1_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+            double pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            area = pi * raio * raio;
+
+            Console.WriteLine(area.ToString("f4", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
