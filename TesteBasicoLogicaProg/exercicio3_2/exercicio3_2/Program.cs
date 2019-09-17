85b7c42d0d3928079c2e2f86e95483a3f5cfb3c7
Dario <darioo_dario@hotmail.com>
2019-09-17 00:09:23 -0300

    Exercício 3.2
    Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida. Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).

---
+++ TesteBasicoLogicaProg/exercicio3_2/exercicio3_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, contDentro=0, contFora=0;
+            N = int.Parse(Console.ReadLine());
+            int numeros;
+
+
+            for (int i = 0; i < N; i++)
+            {
+                numeros = int.Parse(Console.ReadLine());
+                if (numeros >= 10 && numeros <= 20)
+                {
+                    contDentro += 1;
+                } else
+                {
+                    contFora += 1;
+                }
+            }
+
+            Console.WriteLine(contDentro + " in");
+            Console.WriteLine(contFora + " out");
+        }
+    }
+}
