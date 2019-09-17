b5e70c1ca5232d51cd5bb9a0e44a9900de0cab4b
Dario <darioo_dario@hotmail.com>
2019-09-16 23:32:05 -0300

    baskara
    Ler os valores dos três coeficientes "a", "b" e "c" de uma equação do segundo grau (++=0) Em seguida, mostrar os valores das raízes da equação, conforme exemplos, usando a fórmula de Baskara (veja abaixo). Se a equação não possuir raízes (o valor de "a" não pode ser zero, e o valor de "delta" não pode ser negativo), mostrar uma mensagem "Impossivel calcular".

--- TesteBasicoLogicaProg/exercicio2_2/Program.cs
+++ TesteBasicoLogicaProg/exercicio2_2/Program.cs
@@ -11,7 +11,7 @@ namespace exercicio2_2
             double a, b, c;
             double x1, x2;
             string[] dados;
-            
+
             // leitura de dados
             dados = Console.ReadLine().Split(' ');
             a = double.Parse(dados[0], CultureInfo.InvariantCulture);
@@ -19,18 +19,18 @@ namespace exercicio2_2
             c = double.Parse(dados[2], CultureInfo.InvariantCulture);
 
             // fórmula
-            double delta = (b * b) - 4 * a * c;
+            double delta = b * b - (4 * a * c);
 
-            if ( a <= 0 || delta < 0)
+            if (a == 0 || delta < 0) // se o termo "a" for igual a  0 ou o resultado de delta for negativo, naõ é possivel calcular
             {
                 Console.WriteLine("Impossivel calcular");
             }
             else
             {
-                x1 = -b + Math.Sqrt(delta);
-                x2 = -b - Math.Sqrt(delta);
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
                 Console.WriteLine("X1 = " + x1.ToString("f5", CultureInfo.InvariantCulture));
-                Console.WriteLine("X2 = " + x1.ToString("f5", CultureInfo.InvariantCulture));
+                Console.WriteLine("X2 = " + x2.ToString("f5", CultureInfo.InvariantCulture));
 
             }
             Console.ReadLine();
